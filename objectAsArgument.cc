1b5c26ed0b596378ed836bf9863205dea9051031
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-08 11:27:19 +0530

    this

--- objectAsArgument.cc
+++ objectAsArgument.cc
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Time{
+class time{
     int hours;
     int minutes;
     public:
@@ -26,16 +26,15 @@ void time :: sum(time t1, time t2){
     hours = hours + t1.hours +t2.hours; 
 }
 int main(){
-    time T1,T2,T3;
+    time T1, T2, T3;
+    
     T1.getTime(2,45);
     T2.getTime(3,30);
 
     T3.sum(T1,T2);
-    cout<<"T1 = ";
-    T1.putTime();
-    cout<<"T2 = ";
-    T2.putTime();
-    cout<<"T3 = ";
-    T3.putTime();
+
+    cout<<"T1 = ";  T1.putTime();
+    cout<<"T2 = ";  T2.putTime();
+    cout<<"T3 = ";  T3.putTime();
     return 0;
 }
